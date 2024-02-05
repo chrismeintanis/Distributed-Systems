66ad30adfe14ba97646a327b0492ffc65746dce6
chrismeintanis <93153111+chrismeintanis@users.noreply.github.com>
2024-02-05 23:32:50 +0200

    Delete Distributed Systems/distributed-systems directory

--- Distributed Systems/distributed-systems/src/main/java/com/distributed/systems/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.distributed.systems.repository;
-
-import com.distributed.systems.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-    Role findByName(String name);
-}
--- Distributed Systems/distributed-systems/src/main/java/com/distributed/systems/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.distributed.systems.repository;
-
-import com.distributed.systems.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String email);
-}
